92272ada4701cfe10d35fc636e45eaba7a9e1a9b
Edan <Edan184@hotmail.com>
2021-11-24 23:10:26 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// Following  three lines typically required for all express servers
+const express = require("express")
+const app = express()
+const PORT = 3000
+
+app.listen(PORT, (req,res) => {
+    console.log("Whas goood: listening on " + PORT)
+})
