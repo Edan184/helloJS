5997f36b92d99c15a96752f2317f5e12088fa7dc
Edan <Edan184@hotmail.com>
2021-11-24 23:40:15 -0800

    Express prints Hello World

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require("express")
 const app = express()
 const PORT = 3000
 
+app.get('/', (req,res) => {
+    res.send('Hello World!')
+})
+
 app.listen(PORT, (req,res) => {
     console.log("Whas goood: listening on " + PORT)
 })
